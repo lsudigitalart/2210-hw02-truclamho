c1b7369c20983876715d75e3eed2e4539f1e505b
Truc Lam Ho <tho12@lsu.edu>
2024-09-11 02:37:35 -0500

    Assignment 2

---
+++ sketch.js
@@ -0,0 +1,247 @@
+// let img;
+
+// function preload() {
+//     img = loadImage('images/lich_11.png');
+// }
+
+function setup () {
+    createCanvas(400, 269);
+
+}
+
+function draw() {
+    background(226, 188, 177);
+
+    // tint(255, 150);
+    // image(img, 0, 0, width, height);
+
+    strokeWeight();
+    fill(185, 184, 192);
+    beginShape();
+    vertex(402, 192);
+    vertex(353, 195);
+    vertex(340,205);
+    vertex(328,209);
+    vertex(317, 210);
+    vertex(306, 207);
+    vertex(302, 208);
+    vertex(293, 235);
+    vertex(289, 260);
+    vertex(404, 262);
+    vertex(402, 192);
+    endShape(CLOSE);
+
+    strokeWeight();
+    fill(185, 184, 192);
+    beginShape();
+    vertex(403, 116);
+    vertex(366, 115);
+    vertex(362, 125);
+    vertex(375, 179);
+    vertex(358, 185);
+    vertex(407, 190);
+    vertex(415, 115);
+    vertex(403, 116);
+    endShape(CLOSE);
+
+    strokeWeight();
+    fill(185, 184, 192);
+    beginShape();
+    vertex(416, 110);
+    vertex(407, -3);
+    vertex(403, -6);
+    vertex(383, 59);
+    vertex(379, 59);
+    vertex(377, 87);
+    vertex(365, 115);
+    vertex(416, 110);
+    endShape(CLOSE);
+
+    stroke(234, 231, 223);
+    strokeWeight(7);
+    beginShape();
+    vertex(342, 200);
+    vertex(400, 200);
+    endShape(CLOSE);
+
+    stroke(234, 231, 223);
+    strokeWeight(18);
+    beginShape();
+    vertex(361, 128);
+    vertex(400, 128);
+    endShape(CLOSE);
+
+
+    strokeWeight(5);
+    stroke(0);
+
+    noFill();
+    bezier(361, 0, 335, 20, 356, 140, 373, 175);
+    strokeWeight(8);
+    bezier(370, 0, 335, 20, 356, 140, 373, 175);
+    strokeWeight(7);
+    bezier(378, 0, 338, 20, 365, 149, 373, 175);
+
+    strokeWeight(0);
+    fill(234, 231, 223);
+    beginShape();
+    vertex(171, -9);
+    vertex(210, 24);
+    vertex(233, 30);
+    vertex(248, 29);
+    vertex(254, 15);
+    vertex(218, -8);
+    vertex(171, -9);
+    endShape(CLOSE);
+    
+    noFill();
+    strokeWeight(5);
+    bezier(370, 180, 285, 250, 310, 150, 270, 250);
+    bezier(300, 207, 280, 260, 290, 290, 288, 268);
+    bezier(177, 29, 214, 44, 220, 48, 250, 35);
+    bezier(177, 29, 214, 44, 220, 48, 250, 35);
+    bezier(256, 170, 254, 193, 261, 190, 266, 202);
+    bezier(363, 115, 380, 92, 380, 75, 380, 59);
+    bezier(355, 40, 367, 31, 392, 36, 401, 41);
+    bezier(355, 40, 367, 31, 392, 36, 401, 41);
+    bezier(279, 22, 255, 12, 241, 6, 226, -7);
+
+    fill(255);
+    bezier(353, 56, 365, 61, 386, 65, 384, 38);
+
+
+    noFill();
+    bezier(109, 240, 113, 230, 118, 228, 129, 217);
+
+    noFill();
+    bezier(320, 193, 300, 175, 240, 200, 296, 190);
+
+    strokeWeight(3);
+    bezier(320, 193, 300, 180, 255, 190, 289, 190);
+
+    strokeWeight(3);
+    bezier(253, 26, 241, 30, 225, 30, 206, 21);
+    bezier(253, 26, 241, 28, 225, 26, 207, 19);
+    bezier(253, 26, 241, 28, 225, 24, 208, 17);
+
+    strokeWeight(3);
+    bezier(195, 18, 205, 11, 210, 7, 218, -8);
+    bezier(195, 16, 205, 9, 210, 4, 218, -8);
+    bezier(195, 18, 207, 11, 213, 7, 218, -8);
+    bezier(185, 16, 194, 11, 200, 5, 204, 0);
+    
+    strokeWeight(14);
+    bezier(195, 3, 180, -1, 168, -2, 177, -4);
+
+    strokeWeight(6);
+    bezier(293, -4, 307, 6, 320, 7, 337, -1);
+
+    strokeWeight(14);
+    bezier(256, 6, 275, -1, 286, -2, 270, -4);
+
+
+    strokeWeight(3);
+    bezier(356, 30, 364, 25, 373, 25, 380, 30);
+    bezier(245, 51, 249, 63, 248, 77, 250, 90);
+    bezier(245, 51, 249, 63, 253, 77, 256, 90);
+    bezier(239, 55, 244, 71, 246, 81, 250, 94);
+    bezier(231, 61, 235, 75, 241, 90, 245, 101);
+    bezier(222, 63, 229, 80, 236, 92, 239, 104);
+    bezier(218, 71, 224, 83, 225, 95, 233, 104);
+
+
+
+    fill(0);
+    noStroke();
+    bezier(353, 56, 390, 60, 383, 13, 351, 40);
+
+    
+
+    fill(0);
+    strokeWeight(20);
+    beginShape();
+    vertex(367, 15);
+    vertex(385, 0);
+    vertex(392, 4);
+    vertex(392, 0);
+    vertex(483, 10);
+    vertex(399, -4);
+    vertex(403, 46);
+    vertex(384, 21);
+    vertex(364, 13);
+    endShape(CLOSE);
+   
+    stroke(0);
+    strokeWeight(20);
+    beginShape();
+    vertex(395, 38);
+    vertex(400, 21);
+    endShape(CLOSE);
+
+    strokeWeight(20);
+    beginShape();
+    vertex(400, 260);
+    vertex(290, 260);
+    endShape(CLOSE);
+
+    strokeWeight(7);
+    beginShape();
+    vertex(400, 193);
+    vertex(352, 193);
+    endShape(CLOSE);
+    
+    strokeWeight(4);
+    beginShape();
+    vertex(400, 115);
+    vertex(360, 115);
+    endShape(CLOSE);
+
+    strokeWeight(2);
+    beginShape();
+    vertex(287, 137);
+    vertex(309, 145);
+    endShape(CLOSE);
+
+    strokeWeight(2);
+    beginShape();
+    vertex(281, 145);
+    vertex(304, 150);
+    endShape(CLOSE);
+
+    strokeWeight(2);
+    beginShape();
+    vertex(273, 148);
+    vertex(290, 153);
+    endShape(CLOSE);
+
+    strokeWeight(2);
+    beginShape();
+    vertex(268, 150);
+    vertex(285, 159);
+    endShape(CLOSE);
+
+    strokeWeight();
+    beginShape();
+    vertex(290, 259);
+    vertex(263, 271);
+    vertex(193, 260);
+    vertex(154, 248);
+    vertex(119, 230);
+    vertex(120, 228);
+    vertex(150, 235);
+    vertex(155, 240);
+    vertex(150, 235);
+    vertex(212,247);
+    vertex(240,256);
+    vertex(260, 265);
+    vertex(288, 256);
+    endShape();
+
+    
+    // Where's my mouse!?
+    fill(255);
+    text(mouseX, 10, 10);
+    text(mouseY, 10, 20);
+
+
+}
